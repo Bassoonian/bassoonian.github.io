9fd4c80b050cba9b560495bef7d7c1585bfbd8b1
Bassoonian <iasperbassoonian@gmail.com>
2020-09-12 00:00:42 +0200

    Minor documentation updates

--- carisittdictionary/documentation.js
+++ carisittdictionary/documentation.js
@@ -17,7 +17,8 @@ function prepDoc()
 
 function loadDocContent(contentid)
 {
-	$.get("documentation/0.html",function(dat){
+	_last_loaded_file=contentid;
+	$.get("documentation/"+contentid+".html",function(dat){
 		dat=docParseData(dat);
 		document.getElementById("doc_main_content").innerHTML=dat;
 		//Make level list
@@ -67,5 +68,35 @@ function loadDocContent(contentid)
 function docParseData(dat)
 {
 	for(var i=0;i<stagelist.length;i++) dat=replaceAll("§lang_"+i,stagelist[i][0],dat);
-	return(dat);
+	if (dat.search("||SOUNDCHANGES||")>-1)
+	{
+		var temp="";
+		var pipo=stagelist[_last_loaded_file][1];
+		var texto="";
+		while(pipo<dbase[0].length)
+		{
+			if (_last_loaded_file!=stagelist.length-1)
+			{
+				if (pipo==stagelist[_last_loaded_file+1][1]) break;
+			}
+			if (dbase[1][pipo].charAt(0)!="!"&&dbase[2][pipo]!="VVV"&&dbase[1][pipo]!="GRAMMAR"&&dbase[1][pipo]!="LOANS")
+			{
+				if (dbase[1][pipo]!="")
+				{
+					if (temp!="") temp+="</pre>";
+					if (texto!="") temp+="<p>"+texto+"</p>";
+					var teetee=dbase[1][pipo].split("|-|");
+					temp+="<p>"+teetee[0]+"</p><pre>";
+					if (teetee.length>1) texto=teetee[1];
+					else texto="";
+				}
+				else temp+="</br>";
+				temp+=dbase[2][pipo];
+			}
+			pipo++;
+		}
+		if (temp!="") temp+="</pre>";
+		dat=dat.replace("||SOUNDCHANGES||",temp);
+	}
+	return(parseEtymoText(dat));
 }
