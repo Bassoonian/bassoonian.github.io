0fcb7e7e5b7978d3ab492b8c88761650162e86ea
Bassoonian <iasperbassoonian@gmail.com>
2020-09-12 21:48:53 +0200

    Reorganisation (file was getting too big)

---
+++ carisittdictionary/constants.js
@@ -0,0 +1,300 @@
+sources=[
+	["ed_greek",
+		"Beekes, Robert. <i>Etymological Dictionary of Greek.</i> Leiden, Koninklijke Brill NV, 2010",[
+			["google",		"https://books.google.be/books?id=4qudQAAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-greek/oclc/587850973"]
+		]],
+	["originsnominal",
+		"Beekes, R. S. P. <i>The origins of the Indo-European nominal inflection.</i> Innsbruck, Institut für Sprachwissenschaft der Universität Innsbruck, 1985",[
+			["google",		"https://books.google.be/books?id=83hjAAAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/origins-of-the-indo-european-nominal-inflection/oclc/1015088980"]
+		]],
+	["etruscan_intro",
+		"Bonfante, Giuliano & Bonfante, Larissa. <i>The Etruscan Language: An Introduction, 2nd ed.</i> Manchester, Manchester University Press, 2002",[
+			["google",		"https://books.google.be/books?id=VWGN6e5Rzf8C"],
+			["worldcat",	"https://www.worldcat.org/title/etruscan-language-an-introduction/oclc/475129395"]
+		]],
+	["ed_iranian",
+		"Cheung, Johnny. <i>Etymological Dictionary of the Iranian Verb.</i> Leiden, Koninklijke Brill NV, 2007",[
+			["google",		"https://books.google.be/books?id=lPhjAAAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-the-iranian-verb/oclc/860423476"]
+		]],
+	["dic_gaulish",
+		"Delamarre, Xavier. <i>Dictionnaire de la langue gauloise : une approche linguistique du vieux-celtique continental.</i> Paris, Editions Errance, 2003",[
+			["google",		"https://books.google.be/books?id=i0QcAQAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/dictionnaire-de-la-langue-gauloise-une-approche-linguistique-du-vieux-celtique-continental/oclc/1038048625"]
+		]],
+	["ed_baltic",
+		"Derksen, Rick. <i>Etymological Dictionary of the Baltic Inherited Lexicon.</i> Leiden, Koninklijke Brill NV, 2015",[
+			["google",		"https://books.google.be/books?id=fILunwEACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-the-baltic-inherited-lexicon/oclc/932186673"]
+		]],
+	["ed_slavic",
+		"Derksen, Rick. <i>Etymological Dictionary of the Slavic Inherited Lexicon.</i> Leiden, Koninklijke Brill NV, 2008",[
+			["google",		"https://books.google.be/books?id=LJFiAAAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-the-slavic-inherited-lexicon/oclc/606382920"]
+		]],
+	["ed_latin",
+		"de Vaan, Michiel. <i>Etymological Dictionary of Latin and the other Italic Languages.</i> Leiden, Koninklijke Brill NV, 2008",[
+			["google",		"https://books.google.be/books?id=ecZ1DwAAQBAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-latin-and-the-other-italic-languages/oclc/962020667"]
+		]],
+	["academia_nymph",
+		"Hyllested, Adam. <i>The Water Spirit: Greek νὐμφη, Sanskrit Rámbhā, Lithuanian Lãumė and Some Other Possibly Related Forms.</i> Copenhagen, 2004",[
+			["academia",	"https://www.academia.edu/1429587/The_Water_Spirit_Greek_n%C3%BDmph%C4%93_Sanskrit_R%C3%A1mbh%C4%81_Lithuanian_L%C3%A3um%C4%97_amd_Some_Other_Possibly_Related_Forms"]
+		]],
+	["ielangcult",
+		"Fortson IV, Benjamin W. <i>Indo-European Language and Culture: An Introduction.</i> Malden/Oxford/Carlton, Blackwell Publishing Ltd, 2004",[
+			["google",		"https://books.google.be/books?id=bSxHgej4tKMC"],
+			["worldcat",	"https://www.worldcat.org/title/indo-european-language-and-culture-an-introduction/oclc/778339290"]
+		]],
+	["pie_handbook",
+		"Klein, Jared et al. <i>Handbook of Comparative and Historical Indo-European Linguistics.</i> Berlin/Boston, Walter de Gruyter GmbH, 2017",[
+			["google",		"https://books.google.be/books?id=QgA3DwAAQBAJ"],
+			["google",		"https://books.google.be/books?id=TVFKDwAAQBAJ"],
+			["google",		"https://books.google.be/books?id=SuR8DwAAQBAJ"],
+			["worldcat",	"https://www.worldcat.org/title/handbook-of-comparative-and-historical-indo-european-linguistics-an-international-handbook/oclc/1051139391"]
+		]],
+	["ed_hittite",
+		"Kloekhorst, Alwin. <i>Etymological Dictionary of the Hittite Inherited Lexicon.</i> Leiden/Bosten, Koninklijke Brill NV, 2008",[
+			["google",		"https://books.google.be/books?id=JmdiAAAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-the-hittite-inherited-lexicon/oclc/781175802"]
+		]],
+	["dic_punic",
+		"Krahmalkov, Charles R. <i>Phoenician-Punic Dictionary.</i> Leuven, Peeters, 2000",[
+			["google",		"https://books.google.be/books?id=bh_jdzIT9GQC"],
+			["worldcat",	"https://www.worldcat.org/title/phoenician-punic-dictionary/oclc/860453014"]
+		]],
+	["ed_germanic",
+		"Kroonen, Guus. <i>Etymological Dictionary of Proto-Germanic.</i> Leiden, Koninklijke Brill NV, 2013",[
+			["google",		"https://books.google.be/books?id=cgmFRAAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-proto-germanic/oclc/851754510"]
+		]],
+	["ed_gothic",
+		"Lehmann, Winfred P. <i>A Gothic Etymological Dictionary.</i> Leiden, E.J. Brill, 1986",[
+			["google",		"https://books.google.be/books?id=39UUAAAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/gothic-etymological-dictionary/oclc/797186062"]
+		]],
+	["dic_egyptian",
+		"Lesko, Leonard H. <i>A dictionary of Late Egyptian, 2nd ed., 2 vols.</i> Providence, B.C. Scribe Publications, 2002 and 2004",[
+			["google",		"https://books.google.be/books?id=8GAqAQAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/dictionary-of-late-egyptian-1/oclc/634365751"]
+		]],
+	["contel_calendar",
+		"Le Contel, Jean-Michel & Verdier, Paul. <i>Un calendrier celtique : le calendrier gaulois de Coligny.</i> Paris, Editions Errance, 1997",[
+			["google",		"https://books.google.be/books?id=H1pmAAAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/calendrier-celtique-le-calendrier-gaulois-de-coligny/oclc/245807086"]
+		]],
+	["intro_egyptian",
+		"Loprieno, Antonio. <i>Ancient Egyptian: A Linguistic Introduction.</i> Cambridge, Cambridge University Press, 1995",[
+			["google",		"https://books.google.be/books?id=kW8Mzji0XRgC"],
+			["worldcat",	"https://www.worldcat.org/title/ancient-egyptian-a-linguistic-introduction/oclc/896426228"]
+		]],
+	["macneill_calendar",
+		"Mac Neill, Eóin. <i>On the Notation and Chronography of the Calendar of Coligny.</i> 1926",[
+			["jstor",		"https://www.jstor.org/stable/30007818"],
+			["google",		"https://books.google.be/books?id=QFdAHAAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/on-the-notation-and-chronography-of-the-calendar-of-coligny/oclc/41962970&referer=brief_results"]
+		]],
+	["insearchof",
+		"Mallory, J. P. <i>In Search of the Indo-Europeans: Language, Archeology and Myth.</i> Thames and Hudson, 1989",[
+			["google",		"https://books.google.be/books?id=wLeYkgEACAAJ"]
+		]],
+	["oxf_intro",
+		"Mallory, J. P. & Adams, D. Q. <i>The Oxford Introduction to Proto-Indo-European and the Proto-Indo-European World.</i> New York, Oxford University Press Inc., 2006",[
+			["google",		"https://books.google.be/books?id=lzilBQAAQBAJ"],
+			["worldcat",	"https://www.worldcat.org/title/oxford-introduction-to-proto-indo-european-and-the-proto-indo-european-world/oclc/901737820"]
+		]],
+	["ed_armenian",
+		"Martirosyan, Hrach K. <i>Etymological Dictionary of the Armenian Inherited Lexicon.</i> Leiden, Koninklijke Brill, 2010",[
+			["google",		"https://books.google.be/books?id=340lAQAAMAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-the-armenian-inherited-lexicon/oclc/742434857"]
+		]],
+	["sketch_albanian",
+		"Matasović, Ranko. <i>A Grammatical Sketch of Albanian for Students of Indo-European.</i> Zagreb, 2018",[
+			["academia",	"https://www.academia.edu/37749523/_2018_RANKO_MATASOVI%C4%86_A_Grammatical_Sketch_of_Albanian_for_Students_of_Indo-European"]
+		]],
+	["sketch_armenian",
+		"Matasović, Ranko. <i>A Grammatical Sketch of Classical Armenian.</i> Zagreb, 2009",[
+			["academia",	"https://www.academia.edu/1748235/An_Introduction_to_Classical_Armenian"]
+		]],
+	["ed_celtic",
+		"Matasović, Ranko. <i>Etymological Dictionary of Proto-Celtic.</i> Leiden, Koninklijke Brill NV, 2009",[
+			["google",		"https://books.google.be/books?id=YN_YPQAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-proto-celtic/oclc/262430534"]
+		]],
+	["ed_celtic_rev",
+		"Matasović, Ranko. <i>Addenda et corrigenda to Ranko Matasović's Etymological Dictionary of Proto-Celtic (Brill, Leiden 2009).</i> Zagreb, 2011",[
+			["academia",	"https://www.academia.edu/1489376/Addenda_et_Corrigenda_to_Etymological_Dictionary_of_Proto-Celtic_Brill_Leiden_2009_"]
+		]],
+	["ed_albanian",
+		"Orel, Vladimir. <i>Albanian Etymological Dictionary.</i> Leiden/Boston/Köln, 1998",[
+			["google",		"https://books.google.be/books?id=yJQYAQAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/albanian-etymological-dictionary/oclc/466999284"]
+		]],
+	["albanian_grammar",
+		"Orel, Vladimir. <i>A concise historical grammar of the Albanian language: reconstruction of Proto-Albanian.</i> Leiden/Boston/Köln, 2000",[
+			["google",		"https://books.google.be/books?id=xvKH56aT5mEC"],
+			["worldcat",	"https://www.worldcat.org/title/a-concise-historical-grammar-of-the-albanian-language-reconstruction-of-proto-albanian/oclc/704530164"]
+		]],
+	["spanhist",
+		"Penny, Ralph. <i>A History of the Spanish Language.</i> Cambridge, Cambridge University Press, 2002",[
+			["google",		"https://books.google.be/books?id=ZjcrhyQlFa0C"],
+			["worldcat",	"https://www.worldcat.org/title/history-of-the-spanish-language/oclc/633641168"]
+		]],
+	["pie_to_pgmc",
+		"Ringe, Don. <i>From Proto-Indo-European to Proto-Germanic.</i> Oxford, Oxford University Press, 2009",[
+			["google",		"https://books.google.be/books?id=ymcm0Lt9sR0C"],
+			["worldcat",	"https://www.worldcat.org/title/from-proto-indo-european-to-proto-germanic/oclc/929323754"]
+		]],
+	["etruscan_rix",
+		"Rix, Helmut. <i>Etruscan.</i> In: <i>Woodard, Roger D. The Ancient Languages of Europe.</i> Cambridge, Cambridge University Press, 2010",[
+			["google",		"https://books.google.be/books?id=aPEENAEp938C"],
+			["worldcat",	"https://www.worldcat.org/title/ancient-languages-of-europe/oclc/706988654"]
+		]],
+	["liv",
+		"Rix, Helmut. <i>LIV, Lexikon der Indogermanischen Verben: die Wurzeln und ihre Primärstammbildungen.</i> Wiesbaden, Dr. Ludwig Reichert Verlag, 2001",[
+			["google",		"https://books.google.be/books?id=WHYPOgAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/lexikon-der-indogermanischen-verben-liv-die-wurzeln-und-ihre-primrstammbildungen/oclc/1014630824"]
+		]],
+	["absoluteconstructions",
+		"Ruppel, Antonia. <i>Absolute Constructions in Early Indo-European.</i> New York, Cambridge University Press, 2013",[
+			["google",		"https://books.google.be/books?id=MGmCI3h0_tEC"],
+			["worldcat",	"https://www.worldcat.org/title/absolute-constructions-in-early-indo-european/oclc/1055608191"]
+		]],
+	["ieverbmorpho",
+		"Shields, Kenneth C. <i>A History of Indo-European Verb Morphology.</i> In: <i>Current Issues in Linguistic Theory.</i> Amsterdam/Philadelphia, John Benjamins Publishing Company, 1992",[
+			["google",		"https://books.google.be/books?id=leNBAAAAQBAJ"],
+			["worldcat",	"https://www.worldcat.org/title/history-of-indo-european-verb-morphology/oclc/802746544"]
+		]],
+	["compgrammar",
+		"Sihler, Andrew L. <i>New Comparative Grammar of Latin and Greek.</i> New York/Oxford, Oxford University Press, 1995",[
+			["google",		"https://books.google.be/books?id=tJh15AcIdLgC"],
+			["worldcat",	"https://www.worldcat.org/title/new-comparative-grammar-of-greek-and-latin/oclc/860485555"]
+		]],
+	["ed_basque",
+		"Trask, R. L. <i>Etymological Dictionary of Basque.</i> Sussex, University of Sussex, 2008",[
+			["worldcat",	"https://www.worldcat.org/title/etymological-dictionary-of-basque/oclc/971735884"]
+		]],
+	["lat_vulg",
+		"Väänänen, Veikko. <i>Introduction au latin vulgaire.</i> Klincksieck, 2006",[
+			["google",		"https://books.google.be/books?id=vATEmAEACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/introduction-au-latin-vulgaire/oclc/889929195"]
+		]],
+	["ed_coptic",
+		"Vycichl, Werner. <i>Dictionnaire étymologique de la langue copte.</i> Leuven, Éditions Peeters, 1983",[
+			["google",		"https://books.google.be/books?id=C80XAQAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/dictionnaire-etymologique-de-la-langue-copte/oclc/995613647"]
+		]],
+	["manual_etruscan",
+		"Wallace, Rex E. <i>Zikh Rasna: A Manual of the Etruscan Language and Inscriptions.</i> Ann Arbor/New York, Beech Stave Press, 2008",[
+			["google",		"https://books.google.be/books?id=KH7pNwAACAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/zikh-rasna-a-manual-of-the-etruscan-language-and-inscriptions/oclc/233283707"]
+		]],
+	["howtokilladragon",
+		"Watkins, Calvert. <i>How to kill a dragon: aspects of Indo-European poetics.</i> New York, Oxford University Press, 1995",[
+			["google",		"https://books.google.be/books?id=Ri4sbTiMKN4C"],
+			["worldcat",	"https://www.worldcat.org/title/how-to-kill-a-dragon-aspects-of-indo-european-poetics/oclc/185513104"]
+		]],
+	["semiticlexicon",
+		"Weninger, Stefan et al. <i>The Semitic Languages: An International Handbook.</i> Berlin/Boston, Walter de Gruyter GmbH & Co. KG, 2011",[
+			["google",		"https://books.google.be/books?id=SMzgBLT87MkC"],
+			["worldcat",	"https://www.worldcat.org/title/semitic-languages-an-international-handbook/oclc/775693894"]
+		]],
+	["ie_poetry_myth",
+		"West, M. L. <i>Indo-European Poetry and Myth.</i> New York, Oxford University Press, 2007",[
+			["google",		"https://books.google.be/books?id=ZXrJA_5LKlYC"],
+			["worldcat",	"https://www.worldcat.org/title/indo-european-poetry-and-myth/oclc/1126614179"]
+		]],
+	["nominaimlexikon",
+		"Wodtko, Dagmar S. et al. <i>Nomina im Indogermanischen Lexikon.</i> Memmingen, Universitätsverlag Winter Heidelberg GmbH, 2008",[
+			["google",		"https://books.google.be/books?id=BBxSAQAAIAAJ"],
+			["worldcat",	"https://www.worldcat.org/title/nomina-im-indogermanischen-lexikon/oclc/778236996"]
+		]]
+];
+glossterms=[
+	["nom"	,"Nominative case"],
+	["acc"	,"Accusative case"],
+	["gen"	,"Genitive case"],
+	["dat"	,"Dative case"],
+	["abl"	,"Ablative case"],
+	["ins"	,"Instrumental case"],
+	["voc"	,"Vocative case"],
+	["loc"	,"Locative case"],
+	["dir"	,"Direct case"],
+	["obl"	,"Oblique case"],
+	["sg"	,"Singular number"],
+	["pl"	,"Plural number"],
+	["act"	,"Active voice"],
+	["pas"	,"Passive voice"],
+	["m"	,"Masculine gender"],
+	["f"	,"Feminine gender"],
+	["n"	,"Neuter gender"],
+	["1"	,"First person"],
+	["2"	,"Second person"],
+	["3"	,"Third person"],
+	["ind"	,"Indicative mood"],
+	["imp"	,"Imperative mood"],
+	["sjv"	,"Subjunctive mood"],
+	["fut"	,"Future"],
+	["pst"	,"Past"],
+	["npst"	,"Non-past"],
+	["neg"	,"Negative"],
+	["pos"	,"Positive"],
+	["pcp"	,"Participle"],
+	["pret"	,"Preterite"],
+	["prs"	,"Present"],
+	["prf"	,"Perfect"],
+	["cnj"	,"Conjunctive"],
+	["prm"	,"Primary"]
+]
+_caseslist=[
+	["nom","voc","acc","gen","abl","dat","loc","ins"],
+	["nom",""   ,"acc","gen",""   ,"dat","loc","ins"],
+	["nom",""   ,"acc","gen",""   ,"dat","loc",""   ],
+	["nom",""	,"acc","gen",""   ,"dat",""   ,""   ],//Loc is still on the sheet, so can be readded if deemed necessary
+	["dir",""	,""   ,""   ,""   ,"dat",""   ,""   ]
+];
+_conjunctivelist=[
+	false,
+	true,
+	true,
+	true,
+	true,
+	true
+];
+
+_voicelist=[
+	["act","pas"],
+	["act","pas"],
+	["act","pas"],
+	["act","pas"]
+];
+
+_genders=[
+	["m","f","n"],
+	["m","f","n"],
+	["m","f","n"],
+	["m","f","n"],
+	["m","f"]
+];
+
+_tamlist=[
+	[["ind",["prs","pret","fut"]],["sjv",["npst","pst"]]],
+	[["ind",["prs","pret","fut"]],["sjv",["npst","pst"]]],
+	[["ind",["prs","pret","fut"]],["sjv",["npst","pst"]]],
+	[["ind",["prs","pret","fut"]],["sjv",["npst","pst"]]]
+];
+
+_vowels=[
+	["a","e","i","o","u","y","ø","ː"],
+	["a","e","i","o","u","y","ø","ː","ã","ẽ","ĩ","õ","ũ","ỹ","ø̃"],
+	["a","e","i","o","u","y","ø","æ","ː","ẽ","õ","ũ"],
+	["a","e","i","o","u","y","ø","æ","ː","̩","%"]
+];
+_illegalonsets=[
+	["rj","lw","km","ks","lb","lm","lp","mb","mv","nd","ng","nk","nr","rf","rg","rk","vn","rgr","rks","rtr","rtw","ktr","rw","rm","lv","ngw","nt","rkn","ln","rstr","rt","rv","mf","mm","nn","nw","rp","nzd","nz","kt","zg","mr","nzg","sj","ns","bd","gd","lk","zd","mvn","rn","jw","nj","mbr","ksm","stw","gm","pj","rb","zn","ltr","rd","rdw","rz","ls","tt","fn","mvr","ksn","lg","mn","nsr","ld","rgw","pt","ft","mbl","rs","wj","wr","lj","vd","ptw","ldw","lt","rzg","ndw","ksl","rzw","gdr","vm","rgj"],
+	["rdj","rj","hs","lw","km","lb","lm","lp","mb","mn","ntj","nd","htl","ng","ngw","nk","rf","rgr","rk","rks","ks","rn","rtl","rp","lvd","vd","rkm","rw","tn","rm","nj","nt","rkn","kn","mbj","rnj","rns","ns","dn","mm","mp","rg","lht","ht","lj","rs","nw","bd","gd","gm","lf","rd","rgj","ln","rt","mbr","rstl","rv","hsm","tm","ntl","rb","bb","ltl","pm","rr","ls","ss","kk","hsn","dm","lg","ntr","ld","rgw","gn","lk","lt","rgm","pn","rdw"],
+	["bb","hs","lb","mb","mn","nk","rf","rgr","rk","rks","ks","rn","rtj","htj","lvd","vd","rkm","km","mbr","ngw","nt","ng","rm","nj","rkn","kn","nd","rw","lj","rnj","rb","rj","dn","lm","mm","rns","ns","rdj","rj","nw","rg","lht","ht","lw","gm","lf","lk","lp","rgj","rv","hsm","rd","ll","ntj","mp","rd","rp","rt","ls","ltr","mbj","kt","hsn","dm","ntr","ld","rgw","rs","rgm","ln","pt","hsl","gn","lg","rr","mbl","rdw"],
+	["bb","rg","nd","lθ","kt","mb"]
+];
---
+++ carisittdictionary/replacescripts.js
@@ -0,0 +1,291 @@
+function getLangName(q)
+	{
+		switch(q)
+		{
+			case "lat": return("Latin");break;
+			case "eus": return("Basque");break;
+			case "peus": return("Proto-Basque");break;
+			case "grc": return("Ancient Greek");break;
+			case "peo": return("Old Persian");break;
+			case "pclt": return("Proto-Celtic");break;
+			case "legp": return("Late Egyptian");break;
+			case "eng": return("English");break;
+			case "pslv": return("Proto-Slavic");break;
+			case "pgmc": return("Proto-Germanic");break;
+			case "sqi": return("Albanian");break;
+			case "toch": return("Tocharian");break;
+			case "tchA": return("Tocharian A");break;
+			case "tchB": return("Tocharian B");break;
+			case "san": return("Sanskrit");break;
+			case "hit": return("Hittite");break;
+			case "hye": return("Armenian");break;
+			case "gal": return("Gaulish");break;
+			case "xpu": return("Punic");break;
+			case "sbi": return("Suebi");break;
+			case "ett": return("Etruscan");break;
+			case "pie": return("Proto-Indo-European");break;
+			case "psmt": return("Proto-Semitic");break;
+			case "mga": return("Middle Irish");break;
+			case "got": return("Gothic");break;
+			case "ang": return("Old English");break;
+			case "purl": return("Proto-Uralice");break;
+			case "sga": return("Old Irish");break;
+			case "cym": return("Welsh");break;
+			case "mhg": return("Middle High German");break;
+			case "gle": return("Irish");break;
+			case "rus": return("Russian");break;
+			case "xlu": return("Luwian");break;
+			default: return(q);break;
+		}
+	}
+function IPAtoXSampa(a)
+	{
+		//Stress, length, etc
+		a=replaceAll("ˈ",'"',a);
+		a=replaceAll("ː",":",a);
+		a=replaceAll("ʲ","_j",a);
+		a=replaceAll("ˠ","_G",a);
+		a=replaceAll("ʰ","_h",a);
+		a=replaceAll("̩","=",a);
+		a=replaceAll("̞","_o",a);
+		a=replaceAll("̃","~",a);
+		a=replaceAll("̯","_^",a);
+		
+		//Consonants
+		a=replaceAll("ɣ","G",a);
+		a=replaceAll("β","B",a);
+		a=replaceAll("ð","D",a);
+		a=replaceAll("ŋ","N",a);
+		a=replaceAll("ʃ","S",a);
+		a=replaceAll("ʁ","R",a);
+		a=replaceAll("ɡ","g",a);
+		a=replaceAll("ɾ","4",a);
+		a=replaceAll("ɲ","J",a);
+		a=replaceAll("θ","T",a);
+		
+		//Vowels
+		a=replaceAll("æ","{",a);
+		a=replaceAll("ø","2",a);
+		a=replaceAll("ɑ","A",a);
+		a=replaceAll("ɛ","E",a);
+		a=replaceAll("ɪ","I",a);
+		a=replaceAll("ɔ","O",a);
+		a=replaceAll("ʊ","U",a);
+		return(a);
+	}
+function apply_orthography(word,stage)
+	{
+		var w=(word.split("~"))[0];
+		w=pronFixOrtho(w,stage);
+		if (stage==0)
+		{
+			w=replaceAll("ks","χ",w);
+			w=replaceAll("nk","γk",w);
+			w=replaceAll("ng","γg",w);
+			
+			w=replaceAll("p","π",w);
+			w=replaceAll("P","Π",w);
+			w=replaceAll("b","β",w);
+			w=replaceAll("B","Β",w);
+			w=replaceAll("t","τ",w);
+			w=replaceAll("T","Τ",w);
+			w=replaceAll("d","δ",w);
+			w=replaceAll("D","Δ",w);
+			w=replaceAll("k","κ",w);
+			w=replaceAll("K","Κ",w);
+			w=replaceAll("g","γ",w);
+			w=replaceAll("G","Γ",w);
+			w=replaceAll("m","μ",w);
+			w=replaceAll("M","Μ",w);
+			w=replaceAll("n","ν",w);
+			w=replaceAll("N","Ν",w);
+			
+			w=replaceAll("f","φ",w);
+			w=replaceAll("F","Φ",w);
+			w=replaceAll("v","ϝ",w);
+			w=replaceAll("V","Ϝ",w);
+			w=replaceAll("s","σ",w);
+			w=replaceAll("S","Σ",w);
+			w=replaceAll("z","ζ",w);
+			w=replaceAll("Z","Ζ",w);
+			w=replaceAll("r","ρ",w);
+			w=replaceAll("R","Ρ",w);
+			w=replaceAll("l","λ",w);
+			w=replaceAll("L","Λ",w);
+			w=replaceAll("j","ι",w);
+			w=replaceAll("J","Ι",w);
+			w=replaceAll("w","υ",w);
+			w=replaceAll("W","Υ",w);
+			w=replaceAll("h","ψ",w);
+			w=replaceAll("H","Ψ",w);
+			
+			//Add back macrons later when you find a way not to make them look like shit lmao
+			w=replaceAll("ā","α",w);
+			w=replaceAll("Ā","Α",w);
+			w=replaceAll("ī","ι",w);
+			w=replaceAll("Ῑ","Ι",w);
+			w=replaceAll("ū","υ",w);
+			w=replaceAll("Ū","Υ",w);
+			
+			w=replaceAll("a","α",w);
+			w=replaceAll("A","Α",w);
+			w=replaceAll("i","ι",w);
+			w=replaceAll("I","Ι",w);
+			w=replaceAll("u","υ",w);
+			w=replaceAll("U","Υ",w);
+			
+			w=replaceAll("ȳ","y",w);
+			w=replaceAll("Ȳ","Y",w);
+			w=replaceAll("ø̄","ευ",w);
+			w=replaceAll("Ø̄","Ευ",w);
+			
+			w=replaceAll("e","ε",w);
+			w=replaceAll("E","Ε",w);
+			w=replaceAll("ē","ει",w);
+			w=replaceAll("Ē","Ει",w);
+			w=replaceAll("ê","η",w);
+			w=replaceAll("Ê","Η",w);
+			w=replaceAll("o","ο",w);
+			w=replaceAll("O","Ο",w);
+			w=replaceAll("ō","ου",w);
+			w=replaceAll("Ō","Ου",w);
+			w=replaceAll("ô","ω",w);
+			w=replaceAll("Ô","Ω",w);
+			
+			w=w.replace(/(σ)($|\s)/g,"ς$2");
+			
+			w="*"+w;
+		}
+		if (stage==1)
+		{
+			w=replaceAll("X","H",w);
+			w=replaceAll("x","h",w);
+			w=replaceAll("̯","",w);
+			w="*"+w;
+		}
+		if (stage==2)
+		{
+			w=replaceAll("ʰ","h",w);
+			w=replaceAll("β","bh",w);
+			w=replaceAll("ð","dh",w);
+			w=replaceAll("ɣ","gh",w);
+			w=replaceAll("^b","B",w);
+			w=replaceAll("^d","D",w);
+			w=replaceAll("^g","G",w);
+		}
+		if (stage==3)
+		{
+			w=replaceAll("θ","th",w);
+			w=replaceAll("ð","dh",w);
+			w=replaceAll("x","kh",w);
+			w=replaceAll("X","Kh",w);
+			w=replaceAll("ɣ","gh",w);
+			
+			w=replaceAll("^t","T",w);
+			w=replaceAll("^d","D",w);
+			w=replaceAll("^g","G",w);
+			
+			//Accents TO FIX!
+			w=w.replace(/([AĀEĒIĪOŌUŪÆØYaāeēiīoōuūæyø̄%])/g,"$1§")
+			var q=w.split("§");
+			
+			
+			for(var i=0;i<q.length;i++)
+			{
+				if (q[i]=="̄"&&i>0)
+				{
+					q[i-1]+="̄";
+					q.splice(i,1);
+					i-=1;
+				}
+			}
+			
+			if (q[q.length-1]=="̄")
+			{
+				q[q.length-2]+="̄";
+				q.pop();
+			}
+			else if (q[q.length-1]=="") q.pop();
+			
+			if (q.length>0)
+			{
+				var ptk=q[q.length-1].replace(/([AĀEĒIĪOŌUŪÆØYaāeēiīoōuūæyø̄%])/g,"!");
+				if (!ptk.includes("!"))
+				{
+					q[q.length-2]+=q[q.length-1];
+					q.pop();
+				}
+			}
+			
+			if (q.length>2) //FIX! 1) don't display if stress is penultimate 2) take antestress into account (muy dificil señor!)
+			{
+				//q[q.length-1]=q[q.length-1].replace(/([mnlr]ˠ?ʲ?)\%/g,"§$1°");
+				q[q.length-2]+="́";
+			}
+			w=q.join("");
+			w=replaceAll("́̄","̄́",w);
+			w=replaceAll("%","",w);
+			
+			w=replaceAll("k","c",w);
+			w=replaceAll("K","C",w);
+			w=replaceAll("w","u",w);
+			w=replaceAll("W","U",w);
+			w=replaceAll("j","i",w);
+			w=replaceAll("J","I",w);
+			w=replaceAll("ŋ","nh",w);
+			
+			//diacritics
+			w=replaceAll("ʲ","̇",w);
+			w=replaceAll("ˠ","̨",w);
+		}
+		return(w);
+	}
+/*function apply_nativealphabet(word,stage) THIS IS A RELIC, left in case it's useful in the future
+	{
+		var w=(word.split("~"))[0];
+		w=pronFixOrtho(w,stage);
+		if (stage==0)
+		{
+			REPLACED!
+			w=replaceAll("st","𐌆",w);
+			w=replaceAll("sk","𐌗",w);
+			
+			w=replaceAll("a","𐌀",w);
+			w=replaceAll("ā","𐌀",w);
+			w=replaceAll("b","𐌁",w);
+			w=replaceAll("d","𐌃",w);
+			w=replaceAll("e","𐌄",w);
+			w=replaceAll("ē","𐌄",w);
+			w=replaceAll("ê","𐌄",w);
+			w=replaceAll("f","𐌅",w);
+			w=replaceAll("v","𐌅",w);
+			w=replaceAll("w","𐌅",w);
+			w=replaceAll("u","𐌅",w);
+			w=replaceAll("ū","𐌅",w);
+			w=replaceAll("g","𐌂",w);
+			w=replaceAll("h","𐌇",w);
+			w=replaceAll("i","𐌉",w);
+			w=replaceAll("ī","𐌉",w);
+			w=replaceAll("j","𐌉",w);
+			w=replaceAll("k","𐌊",w);
+			w=replaceAll("l","𐌋",w);
+			w=replaceAll("m","𐌌",w);
+			w=replaceAll("n","𐌍",w);
+			w=replaceAll("o","𐌏",w);
+			w=replaceAll("ō","𐌏",w);
+			w=replaceAll("ô","𐌏",w);
+			w=replaceAll("p","𐌐",w);
+			w=replaceAll("r","𐌓",w);
+			w=replaceAll("s","𐌔",w);
+			w=replaceAll("z","𐌔",w);
+			w=replaceAll("t","𐌕",w);
+			w=replaceAll("ȳ","𐌖",w);
+			
+			var qqq=Math.floor((Math.random()* 2));
+			if (qqq==0) w=replaceAll("ø̄","𐌄",w);
+			else w=replaceAll("ø̄","𐌏",w);
+			
+			w=((word.split("")).reverse()).join("");
+		}
+		return(w);
+	}*/
