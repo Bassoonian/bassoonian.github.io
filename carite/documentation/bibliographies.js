d331532e2674e041f3fa99dc99bbc258ca3e4181
Bassoonian <iasperbassoonian@gmail.com>
2020-12-28 15:17:44 +0100

    Extra prose, Phoenician&Egyptian finished

--- carite/documentation/bibliographies.js
+++ carite/documentation/bibliographies.js
@@ -3,7 +3,7 @@ _documentation_bibliography=[
 	["0_nom",		"sketch_armenian 5+16-19,ed_armenian 750,oxf_intro 56-62+307-317,compgrammar 244-438,pie_to_pgmc 41-58+64+73+268-291,pie_morph 2-30,jasanoffinstrumentalplural,accusativeplural,ed_celtic 12,ed_latin 5"],
 	["0_vrb",		"oxf_intro 62-65,compgrammar 442-629"],
 	["0_external",	"greek_companion 85-103,etruscan_rix 145-148,gram_punic 19-37,intro_egyptian 28-50+248,ltlu 1742-1761,grcdialects 36-39+43-51"],
-	["0_sc",		"pie_to_pgmc 70"],
+	["0_sc",		"pie_to_pgmc 14-15+67-152,ed_latin 7-8,ed_celtic 6-11"],
 	["mythology",	"oxf_intro 423-441,ie_poetry_myth 120-503"],
 	["holidays",	"macneill_calendar 1-67"],
 	["figures",		"franco xv-xvi,portofling 538"],
